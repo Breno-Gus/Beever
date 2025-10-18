8a8554dcf285db42b38e4b9c1311a3e081bbf1ff
Razawaky <viniciusduarte564@gmail.com>
2025-10-18 18:05:50 -0300

    alteração package

--- src/backend/config/db.js
+++ src/backend/config/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'beever',
-  password: 'beever123_',
+  user: 'root',
+  password: '',
   database: 'beever',
   waitForConnections: true,
   connectionLimit: 10,
